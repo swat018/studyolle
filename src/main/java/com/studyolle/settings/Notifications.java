0cc2ebbb1b29824de476dcb855b3349a0245de7b
jinwoo park <swat018@gmail.com>
2022-05-19 10:18:31 +0900

    feat : 알림 설정

---
+++ src/main/java/com/studyolle/settings/Notifications.java
@@ -0,0 +1,31 @@
+package com.studyolle.settings;
+
+import com.studyolle.domain.Account;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Notifications {
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+
+    public Notifications(Account account) {
+        this.studyCreatedByEmail = account.isStudyCreateByEmail();
+        this.studyCreatedByWeb = account.isStudyCreateByWeb();
+        this.studyEnrollmentResultByEmail = account.isStudyEnrollmentResultByEmail();
+        this.studyEnrollmentResultByWeb = account.isStudyEnrollmentResultByWeb();
+        this.studyUpdatedByEmail = account.isStudyUpdateByEmail();
+        this.studyUpdatedByWeb = account.isStudyUpdateByWeb();
+    }
+}
