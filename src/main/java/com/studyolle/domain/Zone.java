a05a8e3df6e1572fc4a2df235310879ee6ea0ed0
jinwoo park <swat018@gmail.com>
2022-05-24 16:31:46 +0900

    fix : 지역 도메인

--- src/main/java/com/studyolle/domain/Zone.java
+++ src/main/java/com/studyolle/domain/Zone.java
@@ -7,7 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity @Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
 @Builder @AllArgsConstructor @NoArgsConstructor
 public class Zone {
 
