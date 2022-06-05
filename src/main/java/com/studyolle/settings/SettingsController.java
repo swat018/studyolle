47fcb97089560cad47b4fa4612ea577010694b6e
jinwoo park <swat018@gmail.com>
2022-06-06 03:21:36 +0900

    feat : 스터디 설정 - 태그/지역

--- src/main/java/com/studyolle/settings/SettingsController.java
+++ src/main/java/com/studyolle/settings/SettingsController.java
@@ -10,7 +10,10 @@ import com.studyolle.domain.Zone;
 import com.studyolle.settings.form.*;
 import com.studyolle.settings.validator.NicknameValidator;
 import com.studyolle.settings.validator.PasswordFormValidator;
+import com.studyolle.tag.TagForm;
 import com.studyolle.tag.TagRepository;
+import com.studyolle.tag.TagService;
+import com.studyolle.zone.ZoneForm;
 import com.studyolle.zone.ZoneRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -48,6 +51,7 @@ public class SettingsController {
     private final AccountService accountService;
     private final ModelMapper modelMapper;
     private final NicknameValidator nicknameValidator;
+    private final TagService tagService;
     private final TagRepository tagRepository;
     private final ObjectMapper objectMapper;
     private final ZoneRepository zoneRepository;
@@ -159,12 +163,7 @@ public class SettingsController {
     @PostMapping(TAGS + "/add")
     @ResponseBody
     public ResponseEntity addTag(@CurrentAccount Account account, @RequestBody TagForm tagForm) {
-        String title = tagForm.getTagTitle();
-        Tag tag = tagRepository.findByTitle(title);
-        if(tag == null) {
-            tag = tagRepository.save(Tag.builder().title(title).build());
-        }
-
+        Tag tag = tagService.findOrCreateNew(tagForm.getTagTitle());
         accountService.addTag(account, tag);
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/studyolle/study/StudyService.java
+++ src/main/java/com/studyolle/study/StudyService.java
@@ -2,6 +2,8 @@ package com.studyolle.study;
 
 import com.studyolle.domain.Account;
 import com.studyolle.domain.Study;
+import com.studyolle.domain.Tag;
+import com.studyolle.domain.Zone;
 import com.studyolle.study.form.StudyDescriptionForm;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -14,11 +16,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class StudyService {
 
-    private final StudyRepository repositiry;
+    private final StudyRepository repository;
     private final ModelMapper modelMapper;
 
     public Study createNewStudy(Study study, Account account) {
-        Study newStudy = repositiry.save(study);
+        Study newStudy = repository.save(study);
         newStudy.addManager(account);
 
         return newStudy;
@@ -26,18 +28,14 @@ public class StudyService {
 
     public Study getStudyToUpdate(Account account, String path) {
         Study study = this.getStudy(path);
-        if(!account.isManagerOf(study)) {
-            throw new AccessDeniedException("해당 기능을 사용할 수 없습니다.");
-        }
+        checkIfManager(account, study);
 
         return study;
     }
 
     public Study getStudy(String path) {
-        Study study = this.repositiry.findByPath(path);
-        if(study == null) {
-            throw new IllegalArgumentException(path + "에 해당하는 스터디가 없습니다.");
-        }
+        Study study = this.repository.findByPath(path);
+        checkExistingStudy(path, study);
 
         return study;
     }
@@ -56,5 +54,47 @@ public class StudyService {
 
     public void disableStudyBanner(Study study) {
         study.setUseBanner(false);
+    }
+
+    public void addTag(Study study, Tag tag) {
+        study.getTags().add(tag);
+    }
+
+    public void removeTag(Study study, Tag tag) {
+        study.getTags().remove(tag);
+    }
+
+    public void addZone(Study study, Zone zone) {
+        study.getZones().add(zone);
+    }
+
+    public void removeZone(Study study, Zone zone) {
+        study.getZones().remove(zone);
+    }
+
+    public Study getStudyToUpdateTag(Account account, String path) {
+        Study study = repository.findAccountWithTagsByPath(path);
+        checkExistingStudy(path, study);
+        checkIfManager(account, study);
+        return study;
+    }
+
+    public Study getStudyToUpdateZones(Account account, String path) {
+        Study study = repository.findAccountWithZonesByPath(path);
+        checkExistingStudy(path, study);
+        checkIfManager(account, study);
+        return study;
+    }
+
+    private void checkIfManager(Account account, Study study) {
+        if(!account.isManagerOf(study)) {
+            throw new AccessDeniedException("해당 기능을 사용할 수 없습니다.");
+        }
+    }
+
+    private void checkExistingStudy(String path, Study study) {
+        if(study == null) {
+            throw new IllegalArgumentException(path + "에 해당하는 스터디가 없습니다.");
+        }
     }
 }
