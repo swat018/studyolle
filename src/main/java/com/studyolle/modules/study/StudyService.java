7c7a79b15807d1fc0b3155888e561f3b5623f6ac
jinwoo park <swat018@gmail.com>
2022-06-26 22:13:16 +0900

    feat : 알림 - 스터디 개설 알림

--- src/main/java/com/studyolle/modules/study/StudyService.java
+++ src/main/java/com/studyolle/modules/study/StudyService.java
@@ -25,7 +25,6 @@ public class StudyService {
     public Study createNewStudy(Study study, Account account) {
         Study newStudy = repository.save(study);
         newStudy.addManager(account);
-//        eventPublisher.publishEvent(new StudyCreatedEvent(newStudy));
         return newStudy;
     }
 
@@ -110,6 +109,7 @@ public class StudyService {
 
     public void publish(Study study) {
         study.publish();
+        this.eventPublisher.publishEvent(new StudyCreatedEvent(study));
     }
 
     public void close(Study study) {
