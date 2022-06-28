288ec13aa2faf628fb6072724b6b6c224748adf2
jinwoo park <swat018@gmail.com>
2022-06-29 06:06:42 +0900

    feat : 알림 - 관심있는 스터디 변경 알림

--- src/main/java/com/studyolle/modules/study/StudyService.java
+++ src/main/java/com/studyolle/modules/study/StudyService.java
@@ -2,6 +2,7 @@ package com.studyolle.modules.study;
 
 import com.studyolle.modules.account.Account;
 import com.studyolle.modules.study.event.StudyCreatedEvent;
+import com.studyolle.modules.study.event.StudyUpdateEvent;
 import com.studyolle.modules.study.form.StudyDescriptionForm;
 import com.studyolle.modules.study.form.StudyForm;
 import com.studyolle.modules.tag.Tag;
@@ -44,6 +45,7 @@ public class StudyService {
 
     public void updateStudyDescription(Study study, StudyDescriptionForm studyDescriptionForm) {
         modelMapper.map(studyDescriptionForm, study);
+        eventPublisher.publishEvent(new StudyUpdateEvent(study, "스터디 소개를 수정했습니다."));
     }
 
     public void updateStudyImage(Study study, String image) {
@@ -114,14 +116,17 @@ public class StudyService {
 
     public void close(Study study) {
         study.close();
+        eventPublisher.publishEvent(new StudyUpdateEvent(study, "스터디를 종료했습니다."));
     }
 
     public void startRecruit(Study study) {
         study.startRecruit();
+        eventPublisher.publishEvent(new StudyUpdateEvent(study, "팀원 모집을 시작합니다."));
     }
 
     public void stopRecruit(Study study) {
         study.stopRecruit();
+        eventPublisher.publishEvent(new StudyUpdateEvent(study, "팀원 모집을 중단했습니다."));
     }
 
     public boolean isValidPath(String newPath) {
--- src/main/java/com/studyolle/modules/study/event/StudyUpdateEvent.java
+++ src/main/java/com/studyolle/modules/study/event/StudyUpdateEvent.java
@@ -3,11 +3,14 @@ package com.studyolle.modules.study.event;
 import com.studyolle.modules.study.Study;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEvent;
 
 @Getter
 @RequiredArgsConstructor
-public class StudyCreatedEvent {
+public class StudyUpdateEvent{
 
     private final Study study;
+
+    private final String message;
 
 }
