f046b4f7a307ba5c791d8d338c63d79c8f723f3a
jinwoo park <swat018@gmail.com>
2022-07-05 03:09:52 +0900

    feat : 로그인 후 첫 페이지

--- src/main/java/com/studyolle/modules/study/StudyRepository.java
+++ src/main/java/com/studyolle/modules/study/StudyRepository.java
@@ -1,10 +1,14 @@
 package com.studyolle.modules.study;
 
+import com.studyolle.modules.account.Account;
+import com.studyolle.modules.tag.Tag;
+import com.studyolle.modules.zone.Zone;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 @Transactional(readOnly = true)
 public interface StudyRepository extends JpaRepository<Study, Long>, StudyRepositoryExtension {
@@ -33,5 +37,11 @@ public interface StudyRepository extends JpaRepository<Study, Long>, StudyReposi
     @EntityGraph(attributePaths = {"members", "managers"})
     Study findStudyWithManagersAndMembersById(Long id);
 
+    @EntityGraph(attributePaths = {"zones", "tags"})
     List<Study> findFirst9ByPublishedAndClosedOrderByPublishedDateTimeDesc(boolean published, boolean closed);
+
+
+    List<Study> findFirst5ByManagersContainingAndClosedOrderByPublishedDateTimeDesc(Account account, boolean closed);
+
+    List<Study> findFirst5ByMembersContainingAndClosedOrderByPublishedDateTimeDesc(Account account, boolean closed);
 }
